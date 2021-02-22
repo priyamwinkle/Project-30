867a178723f45c8a367e6beec7c7c953a0124416
priyamwinkle <74333305+priyamwinkle@users.noreply.github.com>
2021-02-22 16:52:03 -0500

    Add files via upload

--- Box.js
+++ Box.js
@@ -6,9 +6,11 @@ class Box{
         this.body=Bodies.rectangle(x,y,width,height,options);
         this.width=width;
         this.height=height;
+        this.visibility=255;
         World.add(myWorld,this.body);
     }
-    display(){ 
+display(){ 
+    if(this.body.speed<2){
         var angle = this.body.angle; 
         var pos= this.body.position; 
         push(); 
@@ -17,16 +19,13 @@ class Box{
         rectMode(CENTER); 
         rect(0,0,this.width, this.height); 
         pop(); 
-        if(this.body.speed<2){
+    } 
+    else{ 
+        World.remove(myWorld,this.body); 
+        push(); 
+        this.visibility=this.visibility-5; 
+        tint(255,this.visibility)  
+        pop();
         }
-        else{
-            World.remove(myWorld,this.body);
-            push();
-            this.visibility=this.visibility-5;
-            tint(255,this.visibility)
-            rect(this.body.position.x,this.body.position.y,50,50);
-            pop();
-        }
-        
     }
 }
